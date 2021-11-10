c17d245c2322de44261fccb5ee71709049438625
mitamurakazushi <klx.z1w8.ttk@gmail.com>
2021-11-10 16:58:17 +0900

    work3

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', 'PostController@index');
+Route::post('/posts', 'PostController@store');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+
