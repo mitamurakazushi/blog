4fa7c766b1202aeca4c215104303da0123f19cfc
mitamurakazushi <klx.z1w8.ttk@gmail.com>
2021-11-12 00:46:21 +0900

    commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,15 +6,31 @@
     </head>
     <body>
         <h1>一覧ページ</h1>
-        [<a href='/posts/create'>create</a>]
+        [<a href='/posts/create'>作成</a>]
         <div>
             @foreach ($posts as $post)
+                <div style="border: solid 1px black; padding: 16px;">
                 <a href="/posts/{{ $post->id }}}">{{ $post->title }}</a>
                 <p class='body'>{{ $post->body }}</p>
+                <form action="/posts/{{ $post->id }}" id="delete_form" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="del()">削除</button>
+                </form>
+                <p></p>
+                </div>
             @endforeach    
         </div>
             <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <script>
+            function del(){
+                'use strict';
+                if (confirm("削除しますか？")){
+                document.getElementById("delete_form").submit();
+                }
+            }    
+        </script>
     </body>
 </html>
